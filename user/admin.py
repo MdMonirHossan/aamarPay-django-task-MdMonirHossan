e41b0307cc30fa26e511f6098309618a84f6b63f
MdMonirHossan <mdraju01820@gmail.com>
2025-08-09 12:33:10 +0600

    Working on admin for user app

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+# Create your models here.
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'password1', 'password2')
+        }),
+        ('Permissions', {
+            'fields': ('is_superuser', 'is_staff')
+        })
+    )
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'password', 'date_joined')
+        }),
+        ('Permissions', {
+            'fields': ('is_superuser', 'is_staff')
+        })
+    )
+    list_display    = ['username', 'id' , 'email', 'is_superuser', 'is_staff', 'date_joined', 'last_login', 'is_active']
+    search_fields   = ('email', 'username')
+    ordering        = ('-date_joined',)
+
+# Unregister User form default admin site
+admin.site.unregister(User)
+
+# Register Custom User Admin
+admin.site.register(User, UserAdmin)
