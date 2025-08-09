36e8927472b8c027f2ac4f07fce394fc2c73acba
MdMonirHossan <mdraju01820@gmail.com>
2025-08-09 13:05:40 +0600

    Working on medel and admin in activity_log

--- activity_log/admin.py
+++ activity_log/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ActivityLog
 
 # Register your models here.
+@admin.register(ActivityLog)
+class ActivityLogAdmin(admin.ModelAdmin):
+    list_display    = ['user', 'action', 'description', 'created_at']
+    list_filter     = ['user', 'action']
+    search_fields   = ['user__username', 'action', 'description']
+    readonly_fields = ['user', 'action', 'description', 'metadata', 'created_at']
--- activity_log/models.py
+++ activity_log/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from libs.core.models.base_models import BaseModel
 
 # Create your models here.
+class ActivityLog(BaseModel):
+    """
+    This model/entity is responsible to store various activity logs within the application.
+    """
+    user        = models.ForeignKey(
+                    User, 
+                    related_name='activities', 
+                    on_delete=models.CASCADE, 
+                    help_text='The user who performed the action'
+                )
+    action      = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    metadata    = models.JSONField()
+
+    class Meta:
+        verbose_name_plural = 'Activities'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f"{self.user.username} | {self.action} | {self.created_at.strftime('%Y-%m-%d %H:%M:%s')}"
