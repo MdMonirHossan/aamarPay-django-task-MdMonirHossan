8cb0da9526a9aefe11548a5b9bbab2c68d1648cb
MdMonirHossan <mdraju01820@gmail.com>
2025-08-09 16:57:06 +0600

    Working on permission mixin in admin pannel

--- payment/admin.py
+++ payment/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import FileUpload, PaymentTransaction
+from libs.utils.permissions.staff_permission import ReadOnlyForStaffMixin
 
 # Register your models here.
 @admin.register(FileUpload)
@@ -7,6 +8,7 @@ class FileUploadAdmin(admin.ModelAdmin):
     list_display    = ('user', 'filename', 'status', 'word_count', 'created_at')
     list_filter     = ('filename', 'user', 'status')
     search_fields   = ('filename', 'user__username', 'status')
+    # readonly_fields = ('user', 'file', 'filename', 'status', 'word_count', 'created_at')
 
 
 @admin.register(PaymentTransaction)
@@ -14,3 +16,4 @@ class PaymentTransactionAdmin(admin.ModelAdmin):
     list_display    = ('user', 'transaction_id', 'amount', 'status', 'created_at')
     list_filter     = ('transaction_id', 'user', 'status')
     search_fields   = ('transaction_id', 'user__username', 'status')
+    # readonly_fields = ('user', 'transaction_id', 'amount', 'status', 'gateway_response', 'created_at')
--- user/admin.py
+++ user/admin.py
@@ -9,7 +9,7 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('username', 'email', 'password1', 'password2')
         }),
         ('Permissions', {
-            'fields': ('is_superuser', 'is_staff')
+            'fields': ('is_superuser', 'is_staff', 'groups', 'user_permissions')
         })
     )
     fieldsets = (
@@ -17,7 +17,7 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('username', 'email', 'password', 'date_joined')
         }),
         ('Permissions', {
-            'fields': ('is_superuser', 'is_staff')
+            'fields': ('is_superuser', 'is_staff', 'groups', 'user_permissions')
         })
     )
     list_display    = ['username', 'id' , 'email', 'is_superuser', 'is_staff', 'date_joined', 'last_login', 'is_active']
