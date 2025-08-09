a2a0938d224ce0b555e8534ea78e479a02c48b53
MdMonirHossan <mdraju01820@gmail.com>
2025-08-09 21:28:25 +0600

    Working on File list api

--- payment/urls.py
+++ payment/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import FileListView
 
-urlpatterns = []
+urlpatterns = [
+    path('files', FileListView.as_view(), name='file_list')
+]
--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,20 @@
-from rest_framework.views import APIView
+from rest_framework import views, generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import FileUpload, PaymentTransaction
 from .serializers import FileUploadSerializer, PaymentTransactionSerializer
 
 
 # Create your views here.
+class FileListView(generics.ListAPIView):
+    """
+    This List API view is responsible for providing a list of uploaded files by requested user.
+    """
+    permission_classes = [IsAuthenticated]
+    serializer_class = FileUploadSerializer
+
+    def get_queryset(self):
+        '''
+        Return a queryset of files only for the requested user.
+        '''
+        return FileUpload.objects.filter(user=self.request.user)
