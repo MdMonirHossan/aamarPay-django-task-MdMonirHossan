c560e70e3aa26a5dd153650e68af55852c4d9431
MdMonirHossan <mdraju01820@gmail.com>
2025-08-11 09:44:55 +0600

    Working on signup page and view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import auth, User
 from payment.models import PaymentTransaction, FileUpload
 from activity_log.models import ActivityLog
 
@@ -11,3 +13,26 @@ def dashboard(request):
         'files': user_files
     }
     return render(request, 'dashboard.html', context)
+
+def signup(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            messages.warning(request, 'You are already logged in')
+            return redirect('dashboard')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        re_pass = request.POST['re-password']
+        if password == re_pass:
+            user , created = User.objects.get_or_create(username=username)
+            user.email = email
+            user.set_password(password)
+            user.save()
+            return redirect('login')
+        else:
+            messages.warning(request, "Password doesn't match!")
+    context = {
+        'title': 'Sign Up',
+    }
+    return render(request, 'signup.html', context)
