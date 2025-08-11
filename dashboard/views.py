964b01d6ed6604147d7721814c27fa0f6d935d15
MdMonirHossan <mdraju01820@gmail.com>
2025-08-11 09:07:03 +0600

    Working on dashboard design

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from payment.models import PaymentTransaction, FileUpload
+from activity_log.models import ActivityLog
 
 # Create your views here.
+
 def dashboard(request):
-    context = {}
+    user_files = FileUpload.objects.all()
+    context = {
+        'title': 'Dashboard',
+        'files': user_files
+    }
     return render(request, 'dashboard.html', context)
